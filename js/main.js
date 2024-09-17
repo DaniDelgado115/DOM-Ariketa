9852364353d7c45f973b0579dcd0d958dca7cd9f
Daniel Delgado Eceiza <158839759+DaniDelgado115@users.noreply.github.com>
2024-09-17 18:07:06 +0000

    1.a

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ let hiru= document.getElementById('hiru')
 hiru.innerText('Hiru')
 
 //1. Ariketa
+// a)
+bat.onclick = () => {
+    console.log("click bat botoian")
+    let h1Goiburuak = document.querySelectorAll("h1")
+    for(let i=0; i< h1Goiburuak.length; i++)
+        h1Goiburuak[i].style.color = "red"
+}

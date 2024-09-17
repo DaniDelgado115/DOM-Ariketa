ff03891a1c359820db50b432e6334dada8ca10ee
Daniel Delgado Eceiza <158839759+DaniDelgado115@users.noreply.github.com>
2024-09-17 08:23:05 +0000

    aldaketak

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+let bat= document.getElementById('uno')
+bat.innerText('Bat')
+let bi= document.getElementById('dos')
+bat.innerText('Bi')
+let hiru= document.getElementById('hiru')
+bat.innerText('Hiru')
 
+//1. Ariketa

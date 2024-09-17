8cecc68d5242c31e9a272f87eb26f11b12b130db
Daniel Delgado Eceiza <158839759+DaniDelgado115@users.noreply.github.com>
2024-09-17 17:52:32 +0000

    fghj

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-let bat= document.getElementById('uno')
-bat.innerText('Bat')
-let bi= document.getElementById('dos')
-bi.innerText('Bi')
-let hiru= document.getElementById('hiru')
-hiru.innerText('Hiru')
+
 
 //1. Ariketa

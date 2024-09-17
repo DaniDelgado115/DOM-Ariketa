f667788328f702bdc63113112ce504f09a412dec
Daniel Delgado Eceiza <158839759+DaniDelgado115@users.noreply.github.com>
2024-09-17 19:31:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let bat= document.getElementById('uno')
 bat.innerText('Bat')
 let bi= document.getElementById('dos')
-bat.innerText('Bi')
+bi.innerText('Bi')
 let hiru= document.getElementById('hiru')
-bat.innerText('Hiru')
+hiru.innerText('Hiru')
 
 //1. Ariketa

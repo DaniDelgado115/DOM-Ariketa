7a646ab8c643b6ff9c0ac3030b39c7417865af51
Daniel Delgado Eceiza <158839759+DaniDelgado115@users.noreply.github.com>
2024-09-18 15:59:37 +0000

    c)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload=() => {
     
     document.getElementById('uno').innerText='Bat'
     document.getElementById('dos').innerText='Bi'
-    document.getElementById('tres').innerText='Hiru'
+    document.getElementById('hiru').innerText='Hiru'
 
 //1. Ariketa
 // a)
@@ -17,14 +17,22 @@ window.onload=() => {
 // b)
     let bi=document.getElementById('dos')
     bi.onclick = () => {
-    console.log("click bi botoian")
-    let h2Goiburuak = document.querySelectorAll("h2")
-    let h2Arraya = Array.from(h2Goiburuak)
-    h2Arraya.forEach( (h2elementu, indizea) => {
-        console.log(h2elementu);
-        console.log(indizea);
-        h2elementu.innerText="Goiburu2-"+(indizea+1)
-    })
-}
+        console.log("click bi botoian")
+        let h2Goiburuak = document.querySelectorAll("h2")
+        let h2Arraya = Array.from(h2Goiburuak)
+        h2Arraya.forEach( (h2elementu, indizea) => {
+            console.log(h2elementu);
+            console.log(indizea);
+            h2elementu.innerText="Goiburu2-"+(indizea+1)
+        })
+    }
+// c)
+    let hiru=document.getElementById('hiru')
+    hiru.onclick = () => {
+        let Hak= Array.from(document.querySelectorAll('h1, h2, h3'))
+        let goiburukoak= [];
+        Hak.forEach( (H,ind) => {
+            goiburukoak[ind]=H.outerText})
+        }
 }
 
